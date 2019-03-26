cb33f981528f5b178d10f77d638e1f34e9937099
Preston McGowan <preston.mcgowan@marklogic.com>
2019-03-26 14:00:54 -0400

    Updated AliasTest. Code cleanup on PolygonQueries

--- src/test/java/AliasTest.java
+++ src/test/java/AliasTest.java
@@ -1,45 +1,44 @@
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.RestAssured;
 import org.hamcrest.core.IsNull;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 
-
 public class AliasTest extends AbstractFeatureServiceTest {
 
-    @Test
-    public void testFieldAlias() {
-        String path = "/marklogic/{service}/FeatureServer/{layer}/query?resultRecordCount={resultRecordCount}&orderByFields={orderByFields}&returnGeometry={returnGeometry}";
-                RestAssured
-                .given()
-                  .pathParam("service", "GDeltGKG")
-                  .pathParam("layer", 4)
-                  .pathParam("resultRecordCount", 5)
-                  .pathParam("orderByFields", "name&nbspASC")
-                  .pathParam("returnGeometry", true)
+	@Test
+	public void testFieldAlias() {
+		JsonPath postBody = getJson("testFieldAlias.json");
+		RestAssured
+			.given()
+				.contentType(ContentType.JSON)
+				.body(postBody.prettyPrint())
+			.when()
+				.log().uri()
+				.post(url)
+			.then()
+				.log().ifError()
+				.statusCode(200)
+				.log().ifValidationFails()
+				.body("features.size()", is(5))
 
-                .when()
-                .log().uri()
-                .get(path)
+				.body("features[0].properties.OBJECTID", is(20643))
+				.body("features[0].properties.urlpubtimedate", is("2017-05-24T09:00:00Z"))
+				.body("features[0].properties.urlpubdate", is("2017-05-24Z"))
+				.body("features[0].properties.url", is("http://satnews.com/story.php?number=1191513746"))
+				.body("features[0].properties.name", is("Aalborg, Nordjylland, Denmark"))
 
-                .then()
-                .log().ifError()
-                .statusCode(200)
-                .log().ifValidationFails()
-
-                .body("features.size()", is(5))
-
-                .body("features[0].attributes.OBJECTID", is(20643))
-                .body("features[0].attributes.urlpubtimedate", is(1495616400000l))
-                .body("features[0].attributes.urlpubdate", is(1495584000000l))
-                .body("features[0].attributes.url", is("http://satnews.com/story.php?number=1191513746"))
-                .body("features[0].attributes.name", is("Aalborg, Nordjylland, Denmark"))
-
-                .body( "fields.find {it.name == 'OBJECTID'} .alias", is("OBJECTID"))
-                .body( "fields.find {it.name == 'urlpubtimedate'} .alias", is("pubtime"))
-                .body( "fields.find {it.name == 'urlpubdate'} .alias", is("pubdate"))
-                .body( "fields.find {it.name == 'url'} .alias", is("doc_url"))
-                .body( "fields.find {it.name == 'name'} .alias", is("Location"))
-        ;
-    }
+				.body("metadata.fields.find {it.name == 'OBJECTID'}.type", is("Integer"))
+				.body("metadata.fields.find {it.name == 'OBJECTID'} .alias", is(IsNull.nullValue()))
+				.body("metadata.fields.find {it.name == 'urlpubtimedate'}.type", is("Date"))
+				.body("metadata.fields.find {it.name == 'urlpubtimedate'} .alias", is("pubtime"))
+				.body("metadata.fields.find {it.name == 'urlpubdate'}.type", is("Date"))
+				.body("metadata.fields.find {it.name == 'urlpubdate'} .alias", is("pubdate"))
+				.body("metadata.fields.find {it.name == 'url'}.type", is("String"))
+				.body("metadata.fields.find {it.name == 'url'} .alias", is("doc_url"))
+				.body("metadata.fields.find {it.name == 'name'}.type", is("String"))
+				.body("metadata.fields.find {it.name == 'name'} .alias", is("Location"));
+	}
 }
