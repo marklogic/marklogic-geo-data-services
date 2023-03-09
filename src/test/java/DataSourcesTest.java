10bb0e962752bbf126e0e43212887ed7786304b1
Rob Rudin <rob.rudin@marklogic.com>
2023-03-09 16:33:50 -0500

    Cleaned up test for dataSources
    Renamed "ServiceDescriptorArrayTest" to "DataSourcesTest" so that the next time I'm trying to find the test for data sources and I search for a file named "DataSource..", I find it.

--- src/test/java/DataSourcesTest.java
+++ src/test/java/DataSourcesTest.java
@@ -1,34 +1,38 @@
-import io.restassured.path.json.JsonPath;
+import com.marklogic.gds.GeoQueryRequest;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 
-public class ServiceDescriptorArrayTest extends AbstractFeatureServiceTest {
+public class DataSourcesTest extends AbstractFeatureServiceTest {
+
+    private GeoQueryRequest requestWithLayer(int layerNumber) {
+        return new GeoQueryRequest("DataSourceArrayExample", layerNumber);
+    }
+
     @Test
     public void testViewAsRoot() {
-        JsonPath postBody = getJson("testViewAsRoot.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(5)
+                .recordCount(5)
+                .orderByFields("OBJECTID ASC")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(5))
-
                 .body("features[0].properties.OBJECTID", is(1))
                 .body("features[0].properties.url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
                 .body("features[0].properties.OBJECT_ID", is(1))
-                .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
-        ;
+                .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"));
     }
 
     @Test
     public void testDataSourceWithViewAsRoot() {
-        JsonPath postBody = getJson("testDataSourceWithViewAsRoot.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(6)
+                .recordCount(5)
+                .orderByFields("OBJECTID ASC")
+        )
                     .body(isValidFeatureCollection())
-
                     .body("features.size()", is(5))
-
                     .body("features[0].properties.OBJECTID", is(1))
                     .body("features[0].properties.urlpubtimedate", is("2017-05-24T06:00:00Z"))
                     .body("features[0].properties.urlpubdate", is("2017-05-24Z"))
@@ -37,111 +41,120 @@ public class ServiceDescriptorArrayTest extends AbstractFeatureServiceTest {
                     .body("features[0].properties.urltone", is(-3.41f))
                     .body("features[0].properties.domain", is("rtbf.be"))
                     .body("features[0].properties.urllangcode", is("fra"))
-                    .body("features[0].properties.geores", is(1))
-        ;
+                    .body("features[0].properties.geores", is(1));
     }
 
     @Test
     public void testDataSourcesWithLeftOuterJoin() {
-        JsonPath postBody = getJson("testDataSourcesWithLeftOuterJoin.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(12)
+                .recordCount(5000)
+                .orderByFields("OBJECTID ASC")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(1502))
-
                 .body("features[0].properties.OBJECTID", is(49))
-                .body("features[0].properties.name_join", is("Ukraine"))
-        ;
+                .body("features[0].properties.name_join", is("Ukraine"));
     }
 
+    /**
+     * This test unfortunately takes awhile because it gets back 67k results. Not sure
+     * if that's important to the purpose of the test. 
+     */
     @Test
     public void testDataSourcesWithFullOuterJoin() {
-        JsonPath postBody = getJson("testDataSourcesWithFullOuterJoin.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(13)
+                .recordCount(70000)
+                .orderByFields("OBJECTID ASC")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(67622))
-
                 .body("features[0].properties.OBJECTID", is(49))
-                .body("features[0].properties.name_join", is("Ukraine"))
-        ;
+                .body("features[0].properties.name_join", is("Ukraine"));
     }
 
     @Test
     public void testDataSourcesWithCrossProductJoin() {
-        JsonPath postBody = getJson("testDataSourcesWithCrossProductJoin.json");
-        postQueryForError(postBody);
+        postForResponse(
+            requestWithLayer(14)
+                .orderByFields("OBJECTID ASC")
+                .toString()
+        )
+            // This is apparently due to the "joinType" being intentionally invalid in the layer.
+            // This seems unexpected as the "InvalidJoin" test already tests this.
+            .then().statusCode(500);
     }
 
     @Test
     public void testDataSourcesWithInvalidJoin() {
-        JsonPath postBody = getJson("testDataSourcesWithInvalidJoin.json");
-        postQueryForError(postBody);
+        postForResponse(
+            requestWithLayer(15)
+                .orderByFields("OBJECTID ASC")
+                .toString()
+        )
+            // This is apparently due to the "joinType" being intentionally invalid in the layer.
+            .then().statusCode(500);
     }
 
     @Test
     public void testDataSourceWithViewAsRootAndSparqlJoin() {
-        JsonPath postBody = getJson("testDataSourceWithViewAsRootAndSparqlJoin.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(7)
+                .recordCount(5)
+                .orderByFields("OBJECTID ASC")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(5))
-
                 .body("features[0].properties.OBJECTID", is(1))
                 .body("features[0].properties.url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
                 .body("features[0].properties.OBJECT_ID", is(1))
-                .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
-        ;
+                .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"));
     }
 
     @Test
     public void testDataSourceWithSparqlAsRoot() {
-        JsonPath postBody = getJson("testDataSourceWithSparqlAsRoot.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(8)
+                .recordCount(5)
+                .orderByFields("OBJECTID ASC")
+                .returnGeometry()
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(5))
-
                 .body("features[0].properties.OBJECTID", is(0))
                 .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
-                .body("features[0].properties.DocId", is("/gkg_geojson/gkg_geojson_2017_05_24T02_26_02.zip/gkg_geojson_2017_05_24T02_26_02/0.json"))
-        ;
+                .body("features[0].properties.DocId", is("/gkg_geojson/gkg_geojson_2017_05_24T02_26_02.zip/gkg_geojson_2017_05_24T02_26_02/0.json"));
     }
 
 
 
     @Test
     public void testDataSourceWithSparqlAsRootAndViewJoinWithFieldsElement() {
-        JsonPath postBody = getJson("testDataSourceWithSparqlAsRootAndViewJoinWithFieldsElement.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(9)
+                .recordCount(5)
+                .orderByFields("OBJECTID ASC")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(5))
-
                 .body("features[0].properties.OBJECTID", is(0))
                 .body("features[0].properties.url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
                 .body("features[0].properties.OBJECT_ID", is(0))
-                .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
-        ;
+                .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"));
     }
 
     @Test
     public void testDataSourceWithSparqlAsRootAndViewJoinWithoutFieldsElement() {
-        JsonPath postBody = getJson("testDataSourceWithSparqlAsRootAndViewJoinWithoutFieldsElement.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(10)
+                .recordCount(5)
+                .orderByFields("OBJECTID ASC")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("features.size()", is(5))
-
                 .body("features[0].properties.OBJECT_ID", is(0))
                 .body("features[0].properties.sparql_url", is("https://www.rtbf.be/info/monde/detail_le-suspect-de-manchester-un-etudiant-reserve-issu-d-un-quartier-modeste?id=9615589"))
-
                 .body("features[0].properties.OBJECTID", is(0))
                 .body("features[0].properties.urlpubtimedate", is("2017-05-24T06:00:00Z"))
                 .body("features[0].properties.urlpubdate", is("2017-05-24Z"))
@@ -150,29 +163,20 @@ public class ServiceDescriptorArrayTest extends AbstractFeatureServiceTest {
                 .body("features[0].properties.urltone", is(-3.41f))
                 .body("features[0].properties.domain", is("rtbf.be"))
                 .body("features[0].properties.urllangcode", is("fra"))
-                .body("features[0].properties.geores", is(3))
-        ;
+                .body("features[0].properties.geores", is(3));
     }
 
     @Test
     public void testDataSourceWithSparqlAsRootAndViewJoinWithoutFieldsElementStats() {
-        JsonPath postBody = getJson("testDataSourceWithSparqlAsRootAndViewJoinWithoutFieldsElementStats.json");
-        postQuery(postBody)
-
+        postGeoQueryRequest(
+            requestWithLayer(11)
+                .recordCount(5)
+                .groupByFieldsForStatistics("domain")
+                .withOutStatistics("[ {\"statisticType\":\"count\",\"onStatisticField\":\"OBJECTID\",\"outStatisticFieldName\":\"objectid_count\"} ]")
+        )
                 .body(isValidFeatureCollection())
-
                 .body("statistics.size()", is(2))
-                .body(
-                    "statistics.find { it.domain == '4-traders.com' }.objectid_count",
-                    is(8)
-                )
-                .body(
-                    "statistics.find { it.domain == 'bendigoadvertiser.com.au' }.objectid_count",
-                    is(1)
-                )
-        ;
+                .body("statistics.find { it.domain == '4-traders.com' }.objectid_count", is(8))
+                .body("statistics.find { it.domain == 'bendigoadvertiser.com.au' }.objectid_count", is(1));
     }
-
-
-
 }
